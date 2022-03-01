5cc19eebb1e8e6508619258bf1768995af04ca87
Ravindra Bharathi <ravindrabharathi@users.noreply.github.com>
2022-03-01 19:04:36 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='long-text-token-classification',
+      version='0.01',
+      description='long-text-token-classification',
+      url='https://github.com/ravindrabharathi/long-text-token-classification',
+      author='ravindrabharathi',
+      author_email='srbharathee@gmail.com',
+      license='MIT',
+      packages=['long-text-token-classification'],
+      zip_safe=False)
